c19f9bacf37e33c02c9f22de6ecd99ac66ba2ed3
yangh9596@gmail.com <yangh9596@126.com>
2017-09-10 00:15:52 -0400

    add 231

---
+++ Leetcode/231_Power_of_2.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Add to List 350_Intersection of Two Arrays II
+//
+//  Created by Yang on 9/2/17.
+//  Copyright © 2017 Yang Hu. All rights reserved.
+//  231_Power of 2: Given an integer, write a function to determine if it is a power of two.
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        return (n>0)&&!(n&(n-1)); // be careful of n = 0!!!
+    }
+};
+
+// 2nd solution: sort and two pointers
+
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
