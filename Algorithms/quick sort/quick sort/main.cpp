5a0a691042fd8b38c93ea273752d12e37b3fffa9
yangh9596@gmail.com <yangh9596@126.com>
2017-09-10 15:05:59 -0400

    update quick sort

--- Algorithms/quick sort/quick sort/main.cpp
+++ Algorithms/quick sort/quick sort/main.cpp
@@ -7,15 +7,15 @@
 //
 
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <cstdlib>
 using namespace std;
 
 // Declaration
 int Partition(int* A, int p, int r);
 int RandomPartition(int* A, int p, int r);
 void QuickSort(int* A, int p, int r);
-
+void RandomQuickSort(int* A, int p, int r);
 
 //
 int main() {
@@ -26,14 +26,10 @@ int main() {
         cout << arr[i] << ",";
     cout << endl;
     
-    /*
-     
-     
-     */
+
     
     QuickSort(arr, 0, 9);
     
-    cout << Partition(arr, 0, 9) << endl;
     cout << "Ordered:" << endl;
     for(int i =0; i <= 9;i++)
         cout << arr[i] << ",";
@@ -43,8 +39,8 @@ int main() {
 
 
 
-// Partition function
-int Partition(int* A, int p, int r){
+// Partition function O(n)
+int Partition(int* A, int p, int r){ // take A[r] as the benchmark
     int i = p-1;
     for(int j = p; j <= r-1; j++){
         if(A[j] <= A[r]){
@@ -56,8 +52,8 @@ int Partition(int* A, int p, int r){
     return (i+1);
 }
 
-// Quick sort function
-void QuickSort(int* A, int p, int r){
+// Quick sort function  Average: O(nlog(n)) Worst: O(n^2)
+void QuickSort(int* A, int p, int r){ // T(n) = 2T(T/n)+O(n)
     if(p<r){
         int q = Partition(A, p, r);
         QuickSort(A, p, q-1);
@@ -67,11 +63,15 @@ void QuickSort(int* A, int p, int r){
 
 // Random partition
 int RandomPartition(int* A, int p, int r){
-    int i = 0;
-    return i;
+    int i = rand() % (r-p+1) + p;
+    swap(A[i],A[r]);
+    return Partition(A, p, r);
 }
 
-
-
-
-
+void RandomQuickSort(int* A, int p, int r) {
+    if(p<r){
+        int q = RandomPartition(A, p, r);
+        RandomQuickSort(A, p, q-1);
+        RandomQuickSort(A, q+1, r);
+    }
+}
