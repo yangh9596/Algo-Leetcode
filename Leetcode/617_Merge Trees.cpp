bd552c050d5139864e0a3bb979f7444fbf61aa5a
yangh9596@gmail.com <yangh9596@126.com>
2017-10-26 01:07:32 -0400

    add 627

---
+++ Leetcode/617_Merge Trees.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  Add to List 100 same tree
+//
+//  Created by Lois on 9/2/17.
+//  Copyright © 2017 Yang Hu. All rights reserved.
+//  Merge two binary trees
+
+
+
+#include <iostream>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if ( t1 && t2 ) {
+            TreeNode * root = new TreeNode(t1->val + t2->val);
+            root->left = mergeTrees(t1->left, t2->left);
+            root->right = mergeTrees(t1->right, t2->right);
+            return root;
+        } else {
+            return t1 ? t1 : t2;
+        }
+    }
+};
+
+int main(int argc, const char * argv[]) {
+
+    
+    return 0;
+}
