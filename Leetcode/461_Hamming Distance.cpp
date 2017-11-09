2a242922d8f0dfc62e671713ed592ab0a26b5331
yangh9596@gmail.com <yangh9596@126.com>
2017-11-09 11:36:59 -0500

    add 461

---
+++ Leetcode/461_Hamming Distance.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  461 Hamming Distance
+//
+//  Created by Yolanda on 10/3/17.
+//  Copyright © 2017 Yang Hu. All rights reserved.
+//  The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int num=x^y;
+        //int num=(~(x&y))&(x|y);
+        int dist(0);
+        while(num) {
+            dist++;
+            num &= num-1;
+        }
+        return dist;
+    }
+};
+
+
+int main() {
+    vector<int> example({1,1,0,1});
+    Solution s1;
+    cout << s1.findMaxConsecutiveOnes(example) << endl;
+    return 0;
+}
+
+
+
