6bb0b09f97adb75ed626d302165ad02db42cb522
yangh9596@gmail.com <yangh9596@126.com>
2017-11-08 21:10:21 -0500

    add 20

---
+++ Leetcode/20_Valid Parentheses.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  20_Valid Parentheses
+//
+//  Created by Yolanda on 10/5/17.
+//  Copyright © 2017 Yang Hu. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> paren;
+        for (char& c : s) {
+            switch (c) {
+                case '(':
+                case '{':
+                case '[': paren.push(c); break;
+                case ')': if (paren.empty() || paren.top()!='(') return false; else paren.pop(); break;
+                case '}': if (paren.empty() || paren.top()!='{') return false; else paren.pop(); break;
+                case ']': if (paren.empty() || paren.top()!='[') return false; else paren.pop(); break;
+                default: ; // pass
+            }
+        }
+        return paren.empty() ;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+
+    
+    return 0;
+}
